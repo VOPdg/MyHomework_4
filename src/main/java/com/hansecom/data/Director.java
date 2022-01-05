d14ffcefa38b74521d3761fbacc78cf422786156
veronikapodgol <veronika.podgol@gmail.com>
2022-01-05 15:24:25 +0300

    add my 4th homework

---
+++ src/main/java/com/hansecom/data/Director.java
@@ -0,0 +1,13 @@
+package com.hansecom.data;
+
+public class Director extends School {
+    public Director(){
+        this.age =40;
+        this.name="Larisa Petrovna";
+        this.isStudent=false;
+
+    }
+
+
+
+}
---
+++ src/main/java/com/hansecom/data/School.java
@@ -0,0 +1,22 @@
+package com.hansecom.data;
+
+public class School {
+    protected String name;
+    protected int age;
+    protected boolean isStudent;
+    protected double avgScore;
+    public String getName() {
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public boolean getIsStudent(){
+        return isStudent;
+    }
+    public double getAvgScore(){
+        return avgScore;
+
+    }
+
+}
